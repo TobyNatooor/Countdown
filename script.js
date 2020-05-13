dfd39e1cfffa33ff896fdea0e867db104ae7201d
TobyNatooor <60399231+TobyNatooor@users.noreply.github.com>
2020-05-13 12:33:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 //"May 15, 2020 12:34:00"
 
 window.onload = function () {
-
+6
     setInterval(function () {
         let theTime = new Date();
         let Birthday = new Date("May 13, 2020 12:34:00");
         let theCountdown = document.getElementById("theCountdown");
         let second = ((Birthday.getTime() - theTime.getTime()) / 1000).toFixed(0);
-        console.log(second)
         let minute = (second / 60).toFixed(0);
-        console.log(minute)
         let hour = (second / 3600).toFixed(0);
-        console.log(hour)
         theCountdown.innerHTML = hour + ':' + minute % 60 + ':' + (second % 60);
 
+        let end = document.getElementById("end");
+        if (second == 0 && minute == 0 && hour == 0) {
+            end.innerHTML = "my birthdayyyyy"
+        }
     }, 1000)
 }
